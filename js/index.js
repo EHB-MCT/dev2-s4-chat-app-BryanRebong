a2bee137c8ebe35ac911ca10042738a97764dc66
Bryan Rebong <bryan.rebong@student.ehb.be>
2023-03-15 10:44:49 +0100

    2nd commit

--- js/index.js
+++ js/index.js
@@ -8,6 +8,21 @@ const chat = {
     sendMessage() {
     },
     fetchMessages() {
+        const apiUrl = "https://dev2chat.onrender.com/messages";
+        const apiKey = "your-api-key";
+
+        fetch(apiUrl, {
+        headers: {
+            "Authorization": `Bearer ${apiKey}`
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+        })
+        .catch(error => {
+        console.error(error);
+        });
     },
     renderMessage(message) {
     }
